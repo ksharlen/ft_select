6aa53779b7bb322fd928dacea660c5250bb40097
Kharazim Sharlene <ksharlen@oa-b1.21-school.ru>
2020-02-25 00:23:49 +0300

    done delete mode, need do read only mode

--- src/delete_mode.c
+++ src/delete_mode.c
@@ -6,7 +6,7 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/24 21:23:06 by ksharlen          #+#    #+#             */
-/*   Updated: 2020/02/24 22:18:27 by ksharlen         ###   ########.fr       */
+/*   Updated: 2020/02/25 00:23:26 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,23 +22,17 @@ static void	init_struct_del_mode(struct s_del_mode **sdm)
 
 void	delete_mode(const char *dirname, t_info_args *args)
 {
-	size_t	len_dirname;
-
 	init_struct_del_mode(&args->sdm);
 	args->sdm->dirname = dirname;
 	if (*dirname)
 	{
-		len_dirname = ft_strlen(dirname);
-		if (len_dirname > 3)
-		{
-			//!read only mode
-			if (!ft_memcpy((char *)dirname, "../", 3) ||
-				!ft_memcpy((char *)dirname, "/", 1))
-				args->sdm->status |= READ_FILES;
-			else
-				args->sdm->status |= DELETE_FILES;
-			//!del mode
-		}
+		//!read only mode
+		if (!ft_memcmp((char *)dirname, "../", sizeof(char) * 3) ||
+			!ft_memcmp((char *)dirname, "/", sizeof(char) * 3))
+			args->sdm->status |= READ_FILES;
+		else
+			args->sdm->status |= DELETE_FILES;
+		//!del mode
 	}
 }
 
@@ -50,13 +44,12 @@ void	delete_file(struct s_arg *current, t_info_args *args)
 	ft_strcpy(buf, args->sdm->dirname);
 	ft_strcat(buf, "/");
 	ft_strcat(buf, current->name);
-	// if (remove(buf) == -1)
-	// {
-	// 	ft_printf("%v%sfailed to delete file: %s%s\n", STDERR_FILENO,
-	// 		COLOR_ERROR, buf, FT_COLOR_DFLT);
-	// 	exit(EXIT_FAILURE);
-	// }
-	ft_printf("%s\n", buf);
+	if (remove(buf) == -1)
+	{
+		ft_printf("%v%sfailed to delete file: %s%s\n", STDERR_FILENO,
+			COLOR_ERROR, buf, FT_COLOR_DFLT);
+		exit(EXIT_FAILURE);
+	}
 }
 
 void	delete_files(t_info_args *args)
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 15:26:57 by ksharlen          #+#    #+#             */
-/*   Updated: 2020/02/24 22:17:23 by ksharlen         ###   ########.fr       */
+/*   Updated: 2020/02/25 00:18:38 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ struct termios	g_cpy;
 
 static void	select_mode(t_info_args *args, int argc, char **argv)
 {
+// printf("dirname: %s\n", *argv);
+// exit(EXIT_FAILURE);
 	if (args->status & DEL_MODE)
 	{
 		if (argc == 1)
