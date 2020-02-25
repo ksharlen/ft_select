84338ec4507ac71f25cfec8fedc0db434ae41bce
ksharlen <miily@list.ru>
2020-02-25 22:14:24 +0300

    done search mode

--- src/search_elem.c
+++ src/search_elem.c
@@ -6,7 +6,7 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 21:52:52 by ksharlen          #+#    #+#             */
-/*   Updated: 2020/02/25 22:07:57 by ksharlen         ###   ########.fr       */
+/*   Updated: 2020/02/25 22:13:04 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,29 @@ void	delete_sym_from_search_buf(t_info_args *args)
 		args->s_buf.buf_search[args->s_buf.len_str-- - 1] = 0;
 }
 
+static void	find_a_match(t_info_args *args)
+{
+	struct s_arg *current;
+
+	current = args->begin;
+	while (current)
+	{
+		if (!ft_memcmp(current->name, args->s_buf.buf_search,
+			args->s_buf.len_str))
+		{
+			args->cur_pos = current;
+			break ;
+		}
+		current = current->next;
+	}
+}
+
 void	search_elem(t_info_args *args, t_key sym)
 {
 	if (args->status & SEARCH_MODE)
 	{
 		ft_strcat(args->s_buf.buf_search, (char *)&sym);
 		++args->s_buf.len_str;
+		find_a_match(args);
 	}
 }
